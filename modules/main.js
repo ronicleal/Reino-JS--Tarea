2a774e48c536616f065dd01e0fad88aa8b1d2965
ronicleal <ralealp01@iesalbarregas.es>
2025-10-25 19:32:08 +0200

    Creacion modulo main e incorporacion del metodo showScene✅

---
+++ modules/main.js
@@ -0,0 +1,7 @@
+/*Importacion de clases*/
+import { Jugador } from "./jugadores.js";
+import {Enemigo, JefeFinal} from "./enemigos.js";
+import {mercado, aplicarDescuentoPorRareza} from "./mercado.js";
+import {batalla, agruparPorNivel} from "./ranking.js";
+import { showScene } from "../utils/utils.js";
+
--- utils/utils.js
+++ utils/utils.js
@@ -39,3 +39,17 @@ export const EUR = new Intl.NumberFormat('es-ES', {
   style: 'currency',
   currency: 'EUR'
 });
+
+
+/**
+  * Permite mostrar u ocultar un elemento (escena) de la pantalla.
+  * Selecciona todos los elementos con la clase scene y les remueve la clase active
+  * Selecciona el elemento con el id especificado y le añade la clase active
+  * @param {number} id - identificador del elemento.
+  */
+export function showScene(id) {
+  document.querySelectorAll('.scene').forEach(
+    element => element.classList.remove('active')
+  );
+  document.getElementById(id).classList.add('active');
+}
