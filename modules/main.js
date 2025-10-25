8e3f87d20f918a379a0b6763919f4e932cc931e4
ronicleal <ralealp01@iesalbarregas.es>
2025-10-25 19:40:07 +0200

    Creacion del index.html✅

--- modules/main.js
+++ modules/main.js
@@ -5,3 +5,12 @@ import {mercado, aplicarDescuentoPorRareza} from "./mercado.js";
 import {batalla, agruparPorNivel} from "./ranking.js";
 import { showScene } from "../utils/utils.js";
 
+/*Variables globales*/
+let jugador;
+let enemigos = [];
+let seleccionados = [];
+
+/*Escenas*/
+function escena1(){
+    const scene = document.getElementById
+}
